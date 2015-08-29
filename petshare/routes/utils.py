63859e6dac7ff341c804c27b11a61b67a7d7b59d
Aystub <AustinRiley91@gmail.com>
2015-08-29 08:50:16 -0400

    asdfasdf

--- petshare/routes/utils.py
+++ petshare/routes/utils.py
@@ -3,6 +3,7 @@ from flask import request, json
 from flask.json import JSONEncoder
 from sqlalchemy.ext.declarative import DeclarativeMeta
 from ..errors import APIException
+from .. import db
 
 def validate_parameters(params):
     """Validate that parameters exist in the request JSON or raise an exception."""
@@ -24,7 +25,15 @@ def validate_parameters(params):
 class CustomJSONEncoder(JSONEncoder):
     """JSON encoder to account for various simplifications"""
     def default(self, obj):
-        
+        try:
+            if isinstance(obj, db.Model):
+                temp = obj.to_dict(exclude=getattr(obj, 'json_hidden'))
+                return temp
+
+        except TypeError:
+            pass
+        else:
+            return list(iterable)
 
         try:
             rv = JSONEncoder.default(self, obj)
