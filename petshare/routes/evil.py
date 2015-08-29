cecbe8ed636edbe5fd68cb5e97cd7b80f52bc8ad
Aystub <AustinRiley91@gmail.com>
2015-08-29 06:06:59 -0400

    return response on evil route

--- petshare/routes/evil.py
+++ petshare/routes/evil.py
@@ -7,3 +7,7 @@ evil_bp = Blueprint('evil', __name__, url_prefix='/evil')
 def nuke_it():
     db.reflect()
     db.drop_all()
+
+    rv = jsonify('{message: You have destoyed it you sick bastard.}')
+    rv.status_code = 200
+    return rv
