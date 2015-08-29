2dc1c946b361470afda7a4a64f3afe2424fd02a1
Aystub <AustinRiley91@gmail.com>
2015-08-29 05:46:16 -0400

    fixes

--- petshare/routes/login.py
+++ petshare/routes/login.py
@@ -8,9 +8,34 @@ from ..models import User, AccessToken
 log = logging.getLogger(__name__)
 login_bp = Blueprint('login', __name__, url_prefix='/login')
 
+
+@login_bp.route('/', methods=['POST'])
+def login():
+    validate_parameters(['email', 'password'])
+
+    user = User.query.filter_by(email=request.json.get('email').lower()).first()
+    if not user:
+        raise APIException('User not found', status_code=404)
+
+    if not user.check_password(request.json.get('password')):
+        raise APIException('Bad login information', status_code=401)
+
+    token = AccessToken(user_id=user.id)
+    user.access_token = token.token
+
+    db.session.add(token)
+    db.session.commit()
+
+    g.user = user
+
+    rv = jsonify(user)
+    rv.status_code = 200
+    return rv
+
+
 @login_bp.route('/sign_up', methods=['POST'])
 def sign_up():
-    validate_parameters(['name', 'email', 'phone', 'password'])
+    #validate_parameters(['name', 'email', 'phone', 'password'])
 
     existing_user = User.query.filter_by(email=request.json.get('email').lower()).first()
     if existing_user:
@@ -35,29 +60,5 @@ def sign_up():
     g.user = new_user
 
     rv = jsonify(user=new_user)
-    rv.status_code = 200
-    return rv
-
-
-@login_bp.route('/login', methods=['POST'])
-def login():
-    validate_parameters(['email', 'password'])
-
-    user = User.query.filter_by(email=request.json.get('email').lower()).first()
-    if not user:
-        raise APIException('User not found', status_code=404)
-
-    if not user.check_password(request.json.get('password')):
-        raise APIException('Bad login information', status_code=401)
-
-    token = AccessToken(user_id=user.id)
-    user.access_token = token.token
-
-    db.session.add(token)
-    db.session.commit()
-
-    g.user = user
-
-    rv = jsonify(user)
     rv.status_code = 200
     return rv
